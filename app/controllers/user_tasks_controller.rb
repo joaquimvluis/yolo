4a300f040cda91f9bc62e86b5680c737edefcd1d
Anna-Lena Hof <hofannalena@gmail.com>
2022-09-01 18:07:15 +0100

    worked on dashboard, completed is working as well some frontend stuff

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -5,7 +5,8 @@ class UserTasksController < ApplicationController
     @user = current_user
     # authorize @usertasks
     # authorize @user
-    @usertasks = policy_scope(UserTask).order(:id).includes(task: :experience)
+    @usertasks = policy_scope(UserTask).order(:completed, :id).includes(task: :experience)
+    @completed = UserTask.where(user: current_user, completed: true).count
   end
 
   def completed?
