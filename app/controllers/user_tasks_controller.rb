e45e36fcd3cdb3b3c4b75ff1c1821be196a2826e
Anna-Lena Hof <hofannalena@gmail.com>
2022-09-01 11:10:23 +0100

    added policies to user tasks

--- app/controllers/user_tasks_controller.rb
+++ app/controllers/user_tasks_controller.rb
@@ -1,9 +1,10 @@
 class UserTasksController < ApplicationController
 
   def index
-    @currentusertasks = UserTask.where(user: current_user)
+    # @usertasks = UserTask.where(user: current_user)
     @user = current_user
+    # authorize @usertasks
+    # authorize @user
     @usertasks = policy_scope(UserTask)
   end
-
 end
--- app/policies/user_task_policy.rb
+++ app/policies/user_task_policy.rb
@@ -2,7 +2,8 @@ class UserTaskPolicy < ApplicationPolicy
   class Scope < Scope
     # NOTE: Be explicit about which records you allow access to!
     def resolve
-      scope.all
+      # scope.all
+      scope.where(user: user)
     end
   end
 
