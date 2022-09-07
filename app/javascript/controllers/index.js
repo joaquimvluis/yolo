342ab056188247f4d46717e91e05348e1dc2858e
Anna-Lena Hof <hofannalena@gmail.com>
2022-09-07 15:32:28 +0100

    heroku version

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -7,14 +7,8 @@ import { application } from "./application"
 import CompleteTaskController from "./complete_task_controller"
 application.register("complete-task", CompleteTaskController)
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
-
 import RefreshUsersListController from "./refresh_users_list_controller"
 application.register("refresh-users-list", RefreshUsersListController)
-
-import UpdateKpiCompletedTaskController from "./update_kpi_completed_task_controller"
-application.register("update-kpi-completed-task", UpdateKpiCompletedTaskController)
 
 import KpiController from "./kpi_controller"
 application.register("kpi", KpiController)
