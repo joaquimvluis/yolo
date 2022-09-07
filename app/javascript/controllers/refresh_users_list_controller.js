881f7d7765e1ffe846baa01c4606542fe83a5e4a
joaquimluis <joaquim.mrvl@gmail.com>
2022-09-07 14:34:10 +0100

    made list items clickable and started the routes and method to create new task(unfinished)

--- app/javascript/controllers/refresh_users_list_controller.js
+++ app/javascript/controllers/refresh_users_list_controller.js
@@ -2,20 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="refresh-users-list"
 export default class extends Controller {
-  static targets = ["form", "list", "searchInput"]
-
-  connect() {
-    console.log("refresh users list controller running!")
+  static values = {
+    task: String
   }
+
+  static targets = ["form", "list", "searchInput"]
 
   update(){
     const url = `${this.formTarget.action}?query=${this.searchInputTarget.value}`
-    console.log(url)
     fetch(url, { headers: { 'Accept': 'text/plain' } })
     .then(response => response.text())
     .then((data => {
-      console.log(data)
-     this.listTarget.innerHTML = data;
+      this.listTarget.innerHTML = data;
     }))
+  }
+
+  sendTask(event) {
+    event.preventDefault()
+    const url = event.currentTarget.href + `?task_id=${this.taskValue}`
+    console.log(url)
+    window.location = url
   }
 }
